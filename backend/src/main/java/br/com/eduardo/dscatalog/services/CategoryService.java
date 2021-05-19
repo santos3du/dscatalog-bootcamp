336c130a122a63d42dc92aa6f74447d4819b4c54
Eduardo Alves dos Santos <santosedu@live.com>
2021-05-19 18:54:39 -0300

    Database seeding

--- backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.eduardo.dscatalog.entities.Category;
 import br.com.eduardo.dscatalog.repositories.CategoryRepository;
@@ -13,7 +14,8 @@ public class CategoryService {
 	
 	@Autowired
 	private CategoryRepository repo;
-
+	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repo.findAll();
 	}
